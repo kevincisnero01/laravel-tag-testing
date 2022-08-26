88f64aad07c7bae85d8a9a72ae9922ae83507b35
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-08-25 22:50:42 -0400

    Pruebas con base de datos sqli

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_find()
+    {
+        User::factory()->create([
+            'email' => 'a@admin.com'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'a@admin.com'
+        ]);
+    }
+}
