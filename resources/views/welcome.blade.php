a19a446ea0cfaabeda1c9ee12a9ed7e9cacea4e5
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-08-27 11:48:15 -0400

    Instalar TailwinCSS Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <h4>Listado de etiquetas</h4>
-    <table>
+    <h4>Crear Etiqueta</h4>
+    <form action="tags" method="POST">
+        @csrf
+        <input type="text" name="name" id="name">
+        <input type="submit" value="Agregar">
+    </form>
+    <h4>Listado de Etiquetas</h4>
+    <table border=1>
         @forelse($tags as $tag)
         <tr>
             <td>{{ $tag->name }}</td>
+            <td>
+                <form action="tags/{{$tag->id}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Eliminar">
+                </form>
+            </td>
         </tr>
         @empty 
             <tr>
